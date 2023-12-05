c6c88349f8409e638f58b712089f1602542333a1
Miguel Ángel Ramírez Ruiz <mugui098@gmail.com>
2023-12-05 15:30:09 +0100

    feat: add day1 part 2 solution

--- day1/day1.go
+++ day1/day1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"regexp"
@@ -13,18 +14,55 @@ func check(e error) {
 	}
 }
 
-func getNumberSum(str string) int {
-	re := regexp.MustCompile(`\d`)
-	numStrArray := re.FindAllString(str, -1)
+func parseNumber(str string) int {
+
+	numMap := map[string]int64{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
+	num, err := strconv.ParseInt(str, 0, 64)
+
+	if err != nil {
+		num = numMap[str]
+	}
+
+	return int(num)
+}
+
+func getNumberSum(str string, verbose bool) int {
+	var regexString string
+	if verbose {
+		regexString = `(\d|one|two|three|four|five|six|seven|eight|nine)`
+
+	} else {
+		regexString = `\d`
+	}
+
+	re := regexp.MustCompile(regexString)
+
+	// this helps to catch cases like 'oneight' or 'sevenine'
+	// Go regex does not support lookarounds, which will make this way easier
+	fixedStr := re.ReplaceAllStringFunc(str, func(s string) string {
+		return fmt.Sprintf("%s%s", s, s[len(s)-1:])
+	})
+
+	numStrArray := re.FindAllString(fixedStr, -1)
 
 	if len(numStrArray) == 0 {
 		return 0
 	}
 
-	first, _ := strconv.ParseInt(numStrArray[0], 0, 64)
-	last, _ := strconv.ParseInt(numStrArray[len(numStrArray)-1], 0, 64)
+	res := parseNumber(numStrArray[0])*10 + parseNumber(numStrArray[len(numStrArray)-1])
 
-	return int(first*10 + last)
+	return res
 }
 
 func getStringsFromFile(fileName string) []string {
@@ -33,22 +71,23 @@ func getStringsFromFile(fileName string) []string {
 
 	re := regexp.MustCompile(`.*\n`)
 
-	return re.FindAllString(string(dat), -1)
+	res := re.FindAllString(string(dat), -1)
+
+	return res
 }
 
 func main() {
 	sum := 0
-	args := os.Args[1:]
 
-	if len(args) == 0 {
-		fmt.Println("No file provided as argument (example: go run day1.go <file-with-data>)")
-		os.Exit(42069)
-	}
+	fileName := flag.String("file", "data/data", "file name with the data to consume")
+	verbose := flag.Bool("verbose", true, "considers 'one', 'two', 'three'... as digits")
 
-	stringArray := getStringsFromFile(args[0])
+	flag.Parse()
+
+	stringArray := getStringsFromFile(*fileName)
 
 	for i := 0; i < len(stringArray); i++ {
-		sum += getNumberSum(stringArray[i])
+		sum += getNumberSum(stringArray[i], *verbose)
 	}
 
 	fmt.Println(sum)
